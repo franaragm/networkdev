4bfd63e883cd01044b7bb1468cbe275f9b631275
franaragm <franaragonmesa@gmail.com>
2017-01-05 18:34:47 +0100

    Publicaciones Timeline - Maquinación vista home donde se muestra info perfil básica, bloque timeline y bloque de formulario de publicacion - clase Type para creación de formulario -  Método index en Publication muy parecido al editUser

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -4,11 +4,98 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use AppBundle\Form\PublicationType;
+use BackendBundle\Entity\Publication;
 
 class PublicationController extends Controller
 {
+    private $session;
+
+    /**
+     * PublicationController constructor.
+     */
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
+    /**
+     * Crea formulario con la clase PublicationType y lo renderiza en vista
+     * Comprueba el formulario enviado y guarda los datos
+     *
+     * @param Request $request
+     * @return $this
+     */
     public function indexAction(Request $request)
     {
-        return $this->render('AppBundle:Publication:home.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        $publication = new Publication();
+        $form = $this->createForm(PublicationType::class, $publication);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+
+                // upload image
+                $file = $form['image']->getData();
+                if (!empty($file) && $file != null) {
+                    $ext = $file->guessExtension(); // obtencion de extension
+
+                    if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
+                        $file_name = $user->getId().time().".".$ext;
+                        $file->move("uploads/publications/images", $file_name);
+
+                        $publication->setImage($file_name);
+                    } else {
+                        $publication->setImage(null);
+                    }
+                } else {
+                    $publication->setImage(null);
+                }
+
+                // upload document
+                $doc = $form['document']->getData();
+                if (!empty($doc) && $doc != null) {
+                    $ext = $doc->guessExtension(); // obtencion de extension
+
+                    if ($ext == 'pdf') {
+                        $file_name = $user->getId().time().".".$ext;
+                        $doc->move("uploads/publications/documents", $file_name);
+
+                        $publication->setDocument($file_name);
+                    } else {
+                        $publication->setDocument(null);
+                    }
+                } else {
+                    $publication->setDocument(null);
+                }
+
+                $publication->setUser($user);
+                $publication->setCreatedAt(new \DateTime("now"));
+
+                $em->persist($publication);
+                $flush = $em->flush();
+
+                if ($flush == null) {
+                    $status = 'La publicación se ha creado correctamente !!';
+                } else {
+                    $status = 'Error al añadir la publicación !!';
+                }
+
+            } else {
+                $status = 'La publicación no se ha creado, porque el formulario no es válido';
+            }
+
+            $this->session->getFlashBag()->add("status", $status);
+            return $this->redirectToRoute('home_publications');
+        }
+
+        return $this->render('AppBundle:Publication:home.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
---
+++ src/AppBundle/Form/PublicationType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+
+class PublicationType extends AbstractType
+{
+
+    /**
+     * Define los campos del formulario de publicacion
+     *
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('texto', TextareaType::class, array(
+                'label' => 'Mensaje',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('image', FileType::class, array(
+                'label' => 'Foto',
+                'required' => false,
+                'data_class' => null,
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('document', FileType::class, array(
+                'label' => 'Documento',
+                'required' => false,
+                'data_class' => null,
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('Enviar', SubmitType::class, array(
+                "attr" => array(
+                    "class" => "btn btn-success"
+                )
+            ))
+        ;
+    }
+
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackendBundle\Entity\Publication'
+        ));
+    }
+
+
+    public function getBlockPrefix()
+    {
+        return 'backendbundle_publication';
+    }
+
+
+}
